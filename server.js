6ae6437c06368d9c7d1f51b7bddea423998cfba3
God of War <jamel.eggleston@gmail.com>
2021-10-21 22:32:31 -0400

    added additional routes

--- server.js
+++ server.js
@@ -17,6 +17,15 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/notes', (req, res) => {
+    console.log(req.body);
+  
+});
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
