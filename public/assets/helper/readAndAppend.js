2010e0686634d1125373ffa39d6e0e1cc51820f6
God of War <jamel.eggleston@gmail.com>
2021-10-22 23:26:22 -0400

    added deletenote

--- public/assets/helper/readAndAppend.js
+++ public/assets/helper/readAndAppend.js
@@ -20,4 +20,22 @@ const readAndAppend = (content, file) => {
     });
 };
 
+const deleteNote = (file, passedId) => {
+    fs.readFile(file, 'utf8', (err,data) => {
+      if(err){
+        console.error(err);
+      } else {
+        const ogNotes = JSON.parse(data);
+        let newNotes = ogNotes.filter(({ id }) => id !== passedId);
+        fs.writeFile(file, JSON.stringify(newNotes), (err) => {
+          if(err){
+            console.error(err);
+          } else {
+            console.log('successfully deleted note');
+          }
+        });
+      }
+    });
+  }
+
 module.exports = readAndAppend;
